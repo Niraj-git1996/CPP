953d042a305630371763713df5eb2854a3765d0c
Niraj-git1996 <contact.niraj1996@gmail.com>
2020-05-06 18:44:34 +0530

    stl changes

--- stl/main.cpp
+++ stl/main.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 int main()
 {
-   // vector<int> v={12,67,89,34,6}; /* create a vector object and assign a valur to them */
+   vector<int> v={12,67,89,34,6}; /* create a vector object and assign a valur to them */
     //list<int> v={12,45,20,19,51};/* define same with double linked list */
    // forward_list<int> v={13,27,43,53,33,20};/* define same with single linked list */
-   set<int> v={12,34,65,90,3,38};
+//   set<int> v={12,34,65,90,3,38};
    // v.push_front(38); /* in single list value is push from front */
    // v.push_front(56);
     //v.push_back(24);
@@ -21,7 +21,7 @@ int main()
    // vector<int>::iterator itr;  /* to create iterator  class object to acesss the vector*/
     //list<int>::iterator itr;/*same object creation foe list */
     //forward_list<int>::iterator itr; /*iierator object for the single linked list */
-       set<int>::iterator itr;
+       vector<int>::iterator itr;
     cout<<"using iterator :"<<endl;
     for(itr=v.begin();itr!=v.end();itr++)
         cout<<(*itr)<<" ";
